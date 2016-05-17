5711ccb870cff6d03d23246c5499fe5c9169fe0a
Paul Melnikow <github@paulmelnikow.com>
2016-05-17 09:54:10 -0400

    Pull in the bones so we can focus on the meat (#1)

--- README.md
+++ README.md
@@ -4,3 +4,34 @@ baiji-pod
 Static caching of files from Amazon S3, using [baiji][].
 
 [baiji]: http://github.com/bodylabs/baiji
+
+
+Development
+-----------
+
+```sh
+pip install -r requirements_dev.txt
+rake unittest
+rake lint
+```
+
+
+Contribute
+----------
+
+- Issue Tracker: https://github.com/bodylabs/baiji-pod/issues
+- Source Code: https://github.com/bodylabs/baiji-pod
+
+Pull requests welcome!
+
+
+Support
+-------
+
+If you are having issues, please let us know.
+
+
+License
+-------
+
+The project is licensed under the Apache license, version 2.0.
---
+++ setup.py
@@ -0,0 +1,49 @@
+# https://coderwall.com/p/qawuyq
+# Thanks James.
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+    print 'warning: pandoc or pypandoc does not seem to be installed; using empty long_description'
+
+import os
+requirements_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'requirements.txt')
+with open(requirements_file, 'r') as f:
+    install_requires = [x.strip() for x in f.readlines()]
+
+import importlib
+from setuptools import setup
+
+setup(
+    name='baiji-pod',
+    version=importlib.import_module('baiji.pod').__version__,
+    author='Body Labs',
+    author_email='alex@bodylabs.com, paul.melnikow@bodylabs.com',
+    description='Static caching of files from Amazon S3, using baiji',
+    long_description=long_description,
+    url='https://github.com/bodylabs/baiji-pod',
+    license='Apache',
+    packages=[
+        'baiji',
+        'baiji/pod',
+        'baiji/pod/runners',
+        'baiji/pod/util',
+        'baiji/pod/versioned',
+    ],
+    scripts=[
+        'bin/pod-prefill',
+        'bin/sc',
+        'bin/vc',
+    ],
+    install_requires=install_requires,
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
