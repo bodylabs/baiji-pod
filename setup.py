910dc2de8cb74ec53dea1a379500362f68e40834
Paul Melnikow <paul.melnikow@bodylabs.com>
2016-05-21 17:21:38 -0400

    Try a gross hack

--- setup.py
+++ setup.py
@@ -13,12 +13,33 @@ requirements_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 're
 with open(requirements_file, 'r') as f:
     install_requires = [x.strip() for x in f.readlines()]
 
+def get_version():
+    # This is GROSS! It's to deal with the fact that we can't import anything
+    # until dependencies are installed (cached_property in particular). We see
+    # this when installing the library on a clean CI instance.
+    import os
+    import subprocess
+
+    proc = subprocess.Popen(
+        ['python', '-c', 'import package_version; print package_version.__version__'],
+        cwd=os.path.join(os.path.dirname(__file__), 'baiji/pod'),
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    output, err = proc.communicate()
+    if err:
+        raise err
+    if not proc.returncode == 0:
+        raise ValueError('Expected a version')
+
+    return output.strip()
+
 import importlib
 from setuptools import setup
 
 setup(
     name='baiji-pod',
-    version=importlib.import_module('baiji.pod.package_version').__version__,
+    version=get_version(),
     author='Body Labs',
     author_email='alex@bodylabs.com, paul.melnikow@bodylabs.com',
     description='Static caching of files from Amazon S3, using baiji',
