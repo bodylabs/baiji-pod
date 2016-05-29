7b68fb1d24d93af82db3146575867b9d8c95438a
Paul Melnikow <github@paulmelnikow.com>
2016-05-29 16:36:12 -0400

    Fix prefill (#4)

--- baiji/pod/prefill.py
+++ baiji/pod/prefill.py
@@ -2,9 +2,9 @@ from baiji.util.parallel import ParallelWorker
 
 
 class PrefillWorker(ParallelWorker):
-    def __init__(self, static_cache, versioned_cache, verbose=False):
+    def __init__(self, asset_cache, versioned_cache, verbose=False):
         super(PrefillWorker, self).__init__()
-        self.sc = static_cache
+        self.cache = asset_cache
         self.vc = versioned_cache
         self.verbose = verbose
 
@@ -17,25 +17,25 @@ class PrefillWorker(ParallelWorker):
 
         try:
             if remote.startswith('s3://'):
-                self.sc(remote, verbose=self.verbose)
+                self.cache(remote, verbose=self.verbose)
             else:
                 self.vc(remote, verbose=self.verbose)
         except s3.KeyNotFound:
             print '{} is in the prefill manifest, but is not found!'.format(remote)
 
 
-def prefill(static_cache, versioned_cache, paths, num_processes=None, verbose=False):
+def prefill(asset_cache, versioned_cache, paths, num_processes=None, verbose=False):
     from baiji.util.parallel import parallel_for
     from harrison import Timer
 
     if num_processes is None:
-        num_processes = static_cache.num_prefill_processes
+        num_processes = asset_cache.config.num_prefill_processes
 
     with Timer(verbose=False) as t:
         parallel_for(
             paths,
             PrefillWorker,
-            args=[static_cache, versioned_cache, verbose],
+            args=[asset_cache, versioned_cache, verbose],
             num_processes=num_processes)
 
     print ''
