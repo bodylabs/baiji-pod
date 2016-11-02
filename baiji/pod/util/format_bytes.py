b62ec9e7556c529e25d563097432bbc38c44e5bc
Alex Weiss <algrs@cacography.net>
2016-11-02 11:15:24 -0400

    fix bug in format_bytes where it crashes if given None (#15)

--- baiji/pod/util/format_bytes.py
+++ baiji/pod/util/format_bytes.py
@@ -3,6 +3,8 @@ def format_bytes(num_bytes):
     Sweet little implementation from
     http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
     '''
+    if num_bytes is None:
+        return "0kb"
     for unit in ['b', 'kb', 'mb', 'gb']:
         if num_bytes < 1024.0 and num_bytes > -1024.0:
             return "%3.1f%s" % (num_bytes, unit)
